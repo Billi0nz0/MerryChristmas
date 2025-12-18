470d3404daaae39a8384ae3ab84f8fd56fe1065a
Hezekiah <irochezekiah@gmail.com>
2025-12-18 17:01:16 +0100

    Add files via upload

--- Javascript/greetings.js
+++ Javascript/greetings.js
@@ -1,13 +1,51 @@
-const username = "Champ";
+
+// username
+const nameInput = document.getElementById("usernameInput");
+const saveBtn = document.getElementById("usernameBtn");
+const welcome = document.getElementById("welcomeBack")
+
 
 const greetEl = document.getElementById("greetings");
+
+
+const namePopUp = document.getElementById("namepopUp");
+const nameClose = document.getElementById("name-closepopup")
+
+
+const usernameInput = localStorage.getItem("username");
+    
+if (usernameInput) {
+        welcome.textContent = `Welcome Back ${usernameInput}`
+        namePopUp.style.display = "none"
+    } else {
+            namePopUp.style.display = "block"
+        }
+
+
+
+saveBtn.addEventListener("click", () => {
+    const userName = nameInput.value.trim();
+
+    if(userName !== "") {
+        localStorage.setItem("username", userName);
+        namePopUp.style.display = "none";
+        nameInput.value = ""
+    }
+});
+
+
+
+// greetings
 const currentHour = new Date().getHours();
+    if (currentHour < 12) {
+        greetEl.textContent = `Good Morning!`;
+    } else if (currentHour < 18) {
+        greetEl.textContent = `Good Afternoon!`;
+    } else {
+        greetEl.textContent = `Good Evening!`;
+    }
 
-if (currentHour < 12) {
-    greetEl.textContent = `Good Morning ${username}!`;
-} else if (currentHour < 18) {
-    greetEl.textContent = `Good Afternoon ${username}!`;
-} else {
-    greetEl.textContent = `Good Evening ${username}!`;
-}
-
+// popup
+nameClose.addEventListener("click", ()=>{
+    namePopUp.style.display = "none"
+})
